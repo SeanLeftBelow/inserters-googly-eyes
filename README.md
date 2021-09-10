166c0624afaa6e0eea7410271549d4979ae030df
Sean McCabe <sean@ulation.com>
2021-09-09 18:10:32 -0700

    Updated fork of mod for factorio v1.1

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-### Inserters with eyes
+### Inserters with Googly Eyes
 
-Factorio mod that adds eyes to inserters. Helper script and a set of eyes included for your eye-providing needs.
+Forked from [](https://github.com/vixfwis/inserters-with-eyes)
+> Factorio mod that adds eyes to inserters. Helper script and a set of eyes included for your eye-providing needs.
+
+Not Updated: https://mods.factorio.com/mod/inserters-with-eyes
+Deprecated: https://mods.factorio.com/mod/InserterEyes
+Reddit Threads:
+- https://old.reddit.com/r/factorio/comments/cduy3f/mod_to_add_eyes_to_inserters/
+- https://old.reddit.com/r/factorio/comments/cdiqwk/the_little_arm_that_couldnt_2/
+Removed boblogistics but can add back with request.
+`"dependencies": ["base >= 0.18.0", "? boblogistics >= 0.18.0", "! InserterEyes"]`
+
+## Self Setup
+`apm install language-lua language-factorio-cfg provider-factrio-lua-api`
--- data.lua
+++ data.lua
@@ -1,5 +1,5 @@
 function repl(path)
-  s,n = string.gsub(path, "__(.-)__", "__inserters-with-eyes__")
+  s,n = string.gsub(path, "__(.-)__", "__inserters-googly-eyes__")
   return s
 end
 
@@ -16,12 +16,12 @@ for index, i in ipairs(inserters) do
     end
 end
 
-if mods["boblogistics"] then
-    local bobinserters = {"express-inserter", "express-filter-inserter", "express-stack-inserter", "express-stack-filter-inserter"}
-    for index, i in ipairs(bobinserters) do
-        data.raw["inserter"][i].hand_closed_picture.filename = repl(data.raw["inserter"][i].hand_closed_picture.filename)
-        data.raw["inserter"][i].hand_closed_picture.hr_version.filename = repl(data.raw["inserter"][i].hand_closed_picture.hr_version.filename)
-        data.raw["inserter"][i].hand_open_picture.filename = repl(data.raw["inserter"][i].hand_open_picture.filename)
-        data.raw["inserter"][i].hand_open_picture.hr_version.filename = repl(data.raw["inserter"][i].hand_open_picture.hr_version.filename)
-    end
-end
+-- if mods["boblogistics"] then
+--     local bobinserters = {"express-inserter", "express-filter-inserter", "express-stack-inserter", "express-stack-filter-inserter"}
+--     for index, i in ipairs(bobinserters) do
+--         data.raw["inserter"][i].hand_closed_picture.filename = repl(data.raw["inserter"][i].hand_closed_picture.filename)
+--         data.raw["inserter"][i].hand_closed_picture.hr_version.filename = repl(data.raw["inserter"][i].hand_closed_picture.hr_version.filename)
+--         data.raw["inserter"][i].hand_open_picture.filename = repl(data.raw["inserter"][i].hand_open_picture.filename)
+--         data.raw["inserter"][i].hand_open_picture.hr_version.filename = repl(data.raw["inserter"][i].hand_open_picture.hr_version.filename)
+--     end
+-- end
